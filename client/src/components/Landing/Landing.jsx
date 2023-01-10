2f96d2fb4096e914529a5982b819192a0bfbb085
Tusharbecoding <tusharchopra02122002@gmail.com>
2023-01-10 23:51:34 +0530

    adding clouds

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './Landing.css'
+import clouds from '../../images/clouds1.svg'
 
 function Landing() {
   return (
     <div className="container">
       <div className="title-container">
         <h1 className="title">ECLIPSE</h1>
-      </div>    
+      </div>
+      <div className="cloud-container">
+        <img src={clouds} alt="" />
+      </div>  
     </div>
   )
 }
