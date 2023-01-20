d95eccbc1cc1d4db142e069bd78da71a9c28c64b
Tusharbecoding <tusharchopra02122002@gmail.com>
2023-01-20 23:22:17 +0530

    added header

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Header.css'
+import logo from '../../assets/images/logo.svg'
+
+function Header() {
+  return (
+    <>
+        <div className="header-container">
+            <div className="left-header">
+                <a href="/">ABOUT</a>
+                <a href="/">EVENTS</a>
+                <a href="/">TIMELINE</a>
+            </div>
+            <div className="center-header">
+                
+            </div>
+            <div className="right-header">
+                <a href="/">CONTACT US</a>
+                <a href="/">STORE</a>
+                <a href="/">LOG IN</a>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Header
