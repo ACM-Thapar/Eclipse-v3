75f4436e4a5a09e102bc477cad6f11754b0b4bd3
Harsh <hjain_be21@thapar.edu>
2023-02-20 21:09:15 +0530

    added event components

---
+++ client/src/components/Flamingo/Flamingo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Flamingo/Flamingo.css'
+
+const Flamingo = () => {
+  return (
+    <div>
+      Helo
+    </div>
+  )
+}
+
+export default Flamingo
---
+++ client/src/components/Hacklipse/Hacklipse.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Hacklipse/Hacklipse.css'
+
+const Hacklipse = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Hacklipse
