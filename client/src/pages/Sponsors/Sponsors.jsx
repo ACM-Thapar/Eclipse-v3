9f319022a4925dcfc75ebb3eb922d01b51d5bc72
Anupriya Lathey <anupriyalathey@gmail.com>
2023-03-24 21:38:36 +0530

    added contact form

--- client/src/pages/Sponsors/Sponsors.jsx
+++ client/src/pages/Sponsors/Sponsors.jsx
@@ -1,7 +1,24 @@
-import React from "react";
-import "./Sponsors.css";
-
+import React, {useState} from "react";
+import "./Sponsors.css"; 
+   
 const Sponsors = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [number, setNumber] = useState('');
+    const [emailSent, setEmailSent] = useState(false);
+    const submit = () => {
+        if (name && email && message) {
+           // TODO - send mail
+    
+            setName('');
+            setEmail('');
+            setMessage('');
+            setEmailSent(true);
+        } else {
+            alert('Please fill in all fields.');
+        }
+    }
     return (
             
             <div classname = "sponsors-container">
@@ -20,9 +37,21 @@ const Sponsors = () => {
                 <div className="contact-heading">
                     <h1>Contact Us</h1>
                 </div>
+
+                <div className="contact-form-bg">
+                <div className="contact-form">
+                    <input type="text" placeholder="Name" value={name} onChange={e => setName(e.target.value)} />
+                    <input type="email" placeholder="Email Address" value={email} onChange={e => setEmail(e.target.value)} />
+                    <input type="text" placeholder="Phone Number" value={number} onChange={e => setNumber(e.target.value)} />
+                    <textarea placeholder="Enter your query" value={message} onChange={e => setMessage(e.target.value)}></textarea>
+                    <button onClick={submit}>Submit</button>
                 </div>
+                </div>
+
+            </div>
             </div>
             
           )   
 }
+
 export default Sponsors;
