b135f065eed9b93167c0a2529f9931753ecaaa15
Abhinandan Wadhwa <awadhwa_be22@thapar.edu>
2023-03-24 22:16:04 +0530

    added events page (#42)

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,20 @@ import { Route, Router, Routes } from "react-router-dom";
 import About from "./pages/About";
 import AuthModal from "./components/AuthModal/AuthModal";
 import Header from "./components/Header/Header";
+import Events from "./pages/Events/Events";
+import Hacklipse from "./components/Hacklipse/Hacklipse";
+import Flamingo from "./components/Flamingo/Flamingo";
+import Engima from "./components/Engima/Engima";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/event/hacklipse" element={<Hacklipse />} />
+        <Route path="/event/flamingo" element={<Flamingo />} />
+        <Route path="/event/engima" element={<Engima />} />
         <Route path="/" element={
             <div>
               <Header />
