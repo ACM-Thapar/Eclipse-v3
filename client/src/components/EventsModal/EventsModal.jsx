40cb12b28b9449c60a93f4e638a4f32e8b79a18a
Abhinandan Wadhwa <awadhwa_be22@thapar.edu>
2023-04-03 17:13:23 +0530

    events page responsiveness and modals added

---
+++ client/src/components/EventsModal/EventsModal.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './eventsModal.css';
+import Hacklipse from '../../assets/images/bottomText.svg';
+import Enigma from '../../assets/images/enigma.svg';
+// import Hacklipse from '../../assets/images/bottomText.svg';
+import { FaWindowClose } from 'react-icons/fa';
+
+const EventsModal = ({ closeModal, eventNumber }) => {
+  return (
+    <div className="main-event-container" style={{ position: 'relative' }}>
+      {/* <p>Hacklipse</p> */}
+      {eventNumber === 1 ? <img className='hacklipse-logo' src={Enigma} alt="" />
+      :
+      (eventNumber === 2 ? <img className='hacklipse-logo' src={Hacklipse} alt="" />
+      :
+      <p className='flamingo-heading'>FLAMINGO</p>
+      )
+      }
+      <div className="register-button">
+        <button>Register Here</button>
+      </div>
+      <div className="description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quasi iure eius molestias fugiat ipsum veniam placeat perspiciatis accusantium! Exercitationem nemo, qui culpa esse eius excepturi, ipsam numquam eum dignissimos earum distinctio non? Laudantium? <br /> <br /> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur facere reiciendis dolorum impedit dolorem cumque deleniti. Harum nihil tempora architecto. Quae porro aliquam veniam saepe atque, tempora dolorum harum sint? <br /> <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ea tenetur quaerat voluptatibus veniam blanditiis qui nostrum architecto dolore ad, totam non quis. Non ducimus autem sint voluptatem! Vitae modi, debitis magnam repellat cumque ipsum temporibus. Quidem, autem suscipit? Consequuntur? <br /> <br /> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus magnam quos in harum sequi minima commodi maxime tenetur quam corrupti. Velit officiis incidunt deleniti sint exercitationem! Quo fugit error nostrum.</p>
+      </div>
+      <div onClick={closeModal} className="close"><FaWindowClose size={40} color='#f14242' style={{ position: 'absolute', top: 0, right: 0, cursor: 'pointer' }} /></div>
+    </div>
+  )
+}
+
+export default EventsModal
--- client/src/pages/Events/Events.jsx
+++ client/src/pages/Events/Events.jsx
@@ -1,10 +1,13 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import './events.css';
 import background from '../../assets/images/eventsBackground.png';
 import Menu from '../../components/Menu/Menu.jsx';
-import bottomText from '../../assets/images/bottomText.svg';
+import Hacklipse from '../../assets/images/bottomText.svg';
+import Enigma from '../../assets/images/enigma.svg';
+import { AiOutlineRight, AiOutlineLeft } from 'react-icons/ai';
 
 const Events = () => {
+  const [meshSelected, setMeshSelected] = useState(2);
   return (
     <Suspense>
         <div className='events-container'>
@@ -12,10 +15,16 @@ const Events = () => {
             <img className='background' src={background} alt="" />
             <div className="heading">EVENTS</div>
             <div className="menu">
-                <Menu />
+                <Menu meshSelected={meshSelected} setMeshSelected={setMeshSelected} />
             </div>
             <div className="event-name">
-                <img src={bottomText} alt="" />
+                {
+                  meshSelected === 1 ? (<img className='event-name-img' src={Enigma} alt="" />)
+                  :
+                  meshSelected === 2 ? (<img className='event-name-img' src={Hacklipse} alt="" />)
+                  :
+                  <img className='event-name-img' src={Enigma} alt="" />
+                }
             </div>
         </div>
     </Suspense>
