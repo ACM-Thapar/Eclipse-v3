4f100b1834e9ef0f4f7ed37eb5125c5aeb3bec77
Harsh <hjain_be21@thapar.edu>
2023-04-03 23:43:21 +0530

    made about section responsive

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -8,26 +8,34 @@ const About = () => {
         <>
             <div id="AboutContainer" >
            
-                    <div id="clouds">
+                    {/* <div id="clouds">
                         <img src={cloud_left} alt="" id="left" />
                         <img src={cloud_middle} alt="" id="middle" />
                         <img src={cloud_right} alt="" id="right" />
-                    </div>
+                    </div> */}
                 <span id="AboutHeading" >ABOUT ECLIPSE</span>
                
                 <div id="TextContainer">
 
                     <div id="VerticalSpan"></div>
                     <div id="Text">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum alias inventore magnam perspiciatis nesciunt, fugiat ad consectetur quis animi assumenda reprehenderit quos ea laboriosam adipisci tempore quisquam? Ipsam, temporibus repudiandae.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. At, nostrum! Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, dignissimos!
+                        <br /><br /><br /><br /><br /><br />
+                    Welcome to Eclipse 3.0, the flagship event of the ACM! This week-long celebration of technology and innovation features a diverse range of technical events designed to challenge and inspire students. We have the following 4 events for you this time :
+                    <br /><br />
+                    <b>HACKLIPSE</b> is a 24-hour hackathon where developers, designers, and entrepreneurs work in teams to develop digital products that solve real-world problems using technology. Mentors and experts will be present to provide guidance, and prizes are awarded to the top teams.
+                    <br /><br />
+                    <b>ENGIMA</b> is a competitive programming competition that challenges participants to solve programming problems, ranging from simple to complex, in a timed competition. The winner of the final round receives an internship at a top tech company.
+                    <br /><br />
+                    <b>FLAMINGO</b> is a multi-level design competition that challenges designers to showcase their creativity and innovation in branding, merchandise design, and UI design. Each level has a unique theme, and winners receive prizes.
+                    <br /><br />
+                    <b>KAGGLE</b> Wars is an event for machine learning and artificial intelligence enthusiasts. Participants use their expertise to solve complex problems, learn from industry leaders, and compete for prizes and recognition.
                     </div>
                 </div>
                 </div>
                 
-                <div id="button">
+                {/* <div id="button">
                     <button id="KnowMe">Know me</button>
-                </div>
+                </div> */}
         </>
     )
 }
