65320495d713a689b15505109123cced6d51a331
Tusharbecoding <tusharchopra02122002@gmail.com>
2023-04-04 07:13:42 +0530

    added tracks and prizes section in modal

--- client/src/pages/Prizes/Prizes.jsx
+++ client/src/pages/Prizes/Prizes.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import './tracks.css';
+import './Prizes.css';
 import TracksBG from '../../assets/images/TracksBG.png';
 import Track1 from '../../assets/images/Track1.svg';
 import Track2 from '../../assets/images/Track2.svg';
 import Track3 from '../../assets/images/Track3.svg';
 import Track4 from '../../assets/images/Track4.svg';
 
-const Tracks = () => {
+const Prizes = () => {
   return (
     <div className='tracks-container'>
         
         {/* <img className='bottom-background' src={TracksBG} alt="" /> */}
         <div className="main-tracks-container">
-            <p>TRACKS</p>
+            <p>PRIZES</p>
             <div className="track-box-container-1">
                 <div className="track track-1">
                   <img src={Track1} alt="" />
+                  <div className="track-name">Lorem Ipsum</div>
+                </div>
+                <div className="track track-2">
+                  <img src={Track2} alt="" />
                   <div className="track-name">Lorem Ipsum</div>
                 </div>
                 <div className="track track-2">
@@ -32,10 +36,14 @@ const Tracks = () => {
                   <img src={Track4} alt="" />
                   <div className="track-name">Agriculture</div>
                 </div>
+                <div className="track track-4">
+                  <img src={Track4} alt="" />
+                  <div className="track-name">Agriculture</div>
+                </div>
             </div>
         </div>
     </div>
   )
 }
 
-export default Tracks
+export default Prizes
